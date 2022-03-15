1aba5a003b6f1df4f48817cc210e1f04a0bf66ad
Ha Nguyen <nguyenthithuha177@gmail.com>
2022-03-15 22:48:59 +0700

    register form

---
+++ Buổi 1(2022311)/buoi1_BTVN.php
@@ -0,0 +1,10 @@
+<?php
+
+//BT1: Có hai tổ công nhân, tổ một có 12 công nhân, tổ hai nếu có thêm 4 người thì sẽ gấp đôi tổ một. Hỏi tổ hai có bao nhiêu công nhân?
+
+$to1=12;
+$to2=($to1*2)-4;
+    echo 'So cong nhan to 2 la'.$to2;
+
+//BT2: Cho ban 2000 vnđ đi mua kẹo .Biết : 1 viên kẹo giá 200 vnđ. cứ 2 vỏ kẹo đổi được 1 viên. Hỏi với 2000 vnđ, ban sẽ mua đc bao nhiêu viên kẹo ?
+
---
+++ Buổi 1(2022311)/buoi1_Example.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Homepage</title>
+</head>
+<body>
+<h1>My website</h1>
+<img src="img.jpg" alt="Image">
+<!-- Comment trong html -->
+<p>This is my website</p>
+<?php
+$title = "Đây là website tin tức";
+echo $title;
+// Comment trong php
+/* Comment trong PHP */
+$x = 5;
+$y = 10;
+echo "<br/>";
+echo $x + $y;
+$user_name = "chadluong"; // snake
+$userName = "canhluong";// Camel
+function summary()
+{
+    $z = 15;
+    echo "Test function $z";
+}
+    echo "<br/>";
+    summary();
+
+    function total(){
+
+    }
+?>
+</body>
+</html>
